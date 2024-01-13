be65794fbc75fc2c3e71793c5c6ced313547c3cc
Ksandeepa07 <kaveensandeepa512@gmail.com>
2024-01-14 01:09:51 +0530

    meal api updated

--- src/main/java/com/example/fitnessbackend/service/MealPlanService.java
+++ src/main/java/com/example/fitnessbackend/service/MealPlanService.java
@@ -2,8 +2,10 @@ package com.example.fitnessbackend.service;
 
 import com.example.fitnessbackend.dto.MealPlanDTO;
 import com.example.fitnessbackend.entity.MealPlan;
+import com.example.fitnessbackend.entity.Trainer;
 import com.example.fitnessbackend.repo.MealPlanRepository;
 import com.example.fitnessbackend.util.VarList;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,4 +34,42 @@ public class MealPlanService {
         return modelMapper.map(mealPlans, new TypeToken<ArrayList<MealPlanDTO>>() {
         }.getType());
     }
+
+    public int  updateMealPlan(MealPlanDTO mealPlanDTO) {
+        System.out.println(mealPlanDTO.getMid());
+        MealPlan existingEntity = mealPlanRepository.findById(mealPlanDTO.getMid())
+                .orElseThrow(() -> new EntityNotFoundException("Entity not found with id: " + mealPlanDTO.getMid()));
+
+
+        existingEntity.setMID(mealPlanDTO.getMid());
+        existingEntity.setPlanName(mealPlanDTO.getPlanName());
+        existingEntity.setPlanDetails(mealPlanDTO.getPlanDetails());
+        existingEntity.setCalorieCount(mealPlanDTO.getCalorieCount());
+        // Update other fields as needed
+
+         mealPlanRepository.save(existingEntity);
+
+         return VarList.Created;
+
+
+    }
+
+    public int deleteMealPlan(Integer id) {
+        System.out.println(id);
+        Integer existingEntity = mealPlanRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Entity not found with id: "+id)).getMID();
+
+        mealPlanRepository.deleteById(existingEntity);
+        return VarList.Created;
+
+    }
+
+//    public int deleteMealPlan(MealPlanDTO mealPlanDTO) {
+//
+//        MealPlan existingEntity = mealPlanRepository.findById(mealPlanDTO.getMid())
+//                .orElseThrow(() -> new EntityNotFoundException("Entity not found with id: "+mealPlanDTO.getMid() ));
+//
+//        mealPlanRepository.delete(existingEntity);
+//        return VarList.Created;
+//    }
 }
